0030a8dff30d2d4d54697d8cd44fd3375d0d6803
polar <polar9527@163.com>
2020-07-06 11:58:26 +0800

    change docker tag and fix wrong command

--- build/git.sh
+++ build/git.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 git clone https://github.com/mit-pdos/6.828-qemu.git qemu --depth 1
+cp -R ./qemu ./ubuntu_16.04/
--- build/ubuntu_16.04/build.sh
+++ build/ubuntu_16.04/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker rmi polar9527/ubuntu14.04-mit6.828-2018:1.0
-docker build -t polar9527/ubuntu14.04-mit6.828-2018:1.0 .
+docker rmi polar9527/ubuntu16.04-mit6.828-2018:1.0
+docker build -t polar9527/ubuntu16.04-mit6.828-2018:1.0 .
