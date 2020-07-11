2248e6c0084b4385c600b4bd876e95dd5a61184d
polar <polar9527@163.com>
2020-07-11 10:17:36 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 v=$(pwd)/6.828/
-docker run --rm -it --name xv6 -v "${v}":/6.828 polar9527/ubuntu16.04-mit6.828-2018:1.0 /bin/bash
+docker run -it -d --name xv6 -v "${v}":/6.828 polar9527/ubuntu16.04-mit6.828-2018:1.0 /bin/bash
+docker exec -it xv6 /bin/bash
